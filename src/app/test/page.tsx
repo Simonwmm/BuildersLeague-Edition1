bb22be23198bcf2f118412fc27f039d0100cf12a
Meng Wang <mwang8@academic.rrc.ca>
2024-10-05 01:17:19 -0500

    delete css test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import NewSurvery from "@/components/NewSurvey";
+
+
+export default function pages(){
+    return(
+        <>
+        <NewSurvery></NewSurvery>
+        </>
+    )
+
+}
---
+++ src/components/NewSurvey.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+export default function NewSurvery() {
+    return ( 
+    <div className="container">
+        <h1>New Survey</h1>
+        <button>Back</button>
+        <style>
+        
+        </style>
+
+        <form>
+            <label id="survey-name">Survey Name</label>
+            <input type="text" id="survey-name" placeholder="Survey name" required/>
+
+            <label id="survey-link">Link to Survey</label>
+            <input type="url" id="survey-link" placeholder="Link to Survey" required/> 
+
+            <label id="survey-date">Survey Date</label>
+            <input type="time"/>
+            <input type="date" id="survey-date" required/>
+
+            <label>Target Org</label>
+            <div className="checkbox-group">
+                <label><input type="checkbox" checked/> * </label>
+                <label><input type="checkbox"/> ACME inc</label>
+                <label><input type="checkbox"/> Ori.Gatou</label>
+                <label><input type="checkbox"/> RRC</label>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </div>)
+}
